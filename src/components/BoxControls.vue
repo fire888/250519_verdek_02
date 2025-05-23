7f54e8b5dd85cf61a4509b02a4afe52b83b66dad
vasilii <porokh.vs@gmail.com>
2025-05-23 19:41:27 +0300

    [FEAT]: fix alerts

--- src/components/BoxControls.vue
+++ src/components/BoxControls.vue
@@ -10,17 +10,17 @@
     <fieldset class="border p-3 rounded-lg">
       <legend class="font-semibold mb-2">Размеры коробки</legend>
 
-      <ControlSlider v-model="storeBoxes.boxes[storeBoxes.currentBox].w"
+      <ControlSlider v-model="storeBoxes.boxes[+storeBoxes.currentBox].w"
                      label="Ширина (w)"
                      :min="100"
                      :max="1000"/>
 
-      <ControlSlider v-model="storeBoxes.boxes[storeBoxes.currentBox].d"
+      <ControlSlider v-model="storeBoxes.boxes[+storeBoxes.currentBox].d"
                      label="Высота (h)"
                      :min="100"
                      :max="1000"/>
 
-      <ControlSlider v-model="storeBoxes.boxes[storeBoxes.currentBox].doorRotation"
+      <ControlSlider v-model="storeBoxes.boxes[+storeBoxes.currentBox].doorRotation"
                      label="Открытие двери ()"
                      :min="0"
                      :max="170"/>
@@ -30,12 +30,12 @@
     <fieldset class="border p-3 rounded-lg">
       <legend class="font-semibold mb-2">Боковая фаска&nbsp;(S)</legend>
 
-      <ControlSlider v-model="storeBoxes.boxes[storeBoxes.currentBox].h"
+      <ControlSlider v-model="storeBoxes.boxes[+storeBoxes.currentBox].h"
                      label="Толщина (d)"
                      :min="20"
                      :max="40"/>
 
-      <ControlSelect v-model="storeBoxes.boxes[storeBoxes.currentBox].facetS.type"
+      <ControlSelect v-model="storeBoxes.boxes[+storeBoxes.currentBox].facetS.type"
                      label="Тип"
                      :options="['FACET11','FACET12','FACET13']"/>
     </fieldset>
@@ -44,17 +44,17 @@
     <fieldset class="border p-3 rounded-lg">
       <legend class="font-semibold mb-2">Внутренняя фаска&nbsp;(T)</legend>
 
-      <ControlSlider v-model="storeBoxes.boxes[storeBoxes.currentBox].facetT.offsetX"
+      <ControlSlider v-model="storeBoxes.boxes[+storeBoxes.currentBox].facetT.offsetX"
                      label="Смещение&nbsp;X"
                      :min="0"
                      :max="200"/>
 
-      <ControlSlider v-model="storeBoxes.boxes[storeBoxes.currentBox].facetT.offsetZ"
+      <ControlSlider v-model="storeBoxes.boxes[+storeBoxes.currentBox].facetT.offsetZ"
                      label="Смещение&nbsp;Z"
                      :min="0"
                      :max="200"/>
 
-      <ControlSelect v-model="storeBoxes.boxes[storeBoxes.currentBox].facetT.type"
+      <ControlSelect v-model="storeBoxes.boxes[+storeBoxes.currentBox].facetT.type"
                      label="Тип"
                      :options="['FACET22','FACET33']"/>
     </fieldset>
@@ -63,11 +63,11 @@
     <fieldset class="border p-3 rounded-lg">
       <legend class="font-semibold mb-2">Фаски зеркала</legend>
 
-      <ControlNumber v-model="storeBoxes.boxes[storeBoxes.currentBox].facetInner.count"
+      <ControlNumber v-model="storeBoxes.boxes[+storeBoxes.currentBox].facetInner.count"
                      label="Количество"
                      :min="0" :max="3" :step="1"/>
 
-      <ControlSelect v-model="storeBoxes.boxes[storeBoxes.currentBox].facetInner.rotationY"
+      <ControlSelect v-model="storeBoxes.boxes[+storeBoxes.currentBox].facetInner.rotationY"
                      label="Поворот&nbsp;Y"
                      :options="['0', '90']"/>
     </fieldset>
